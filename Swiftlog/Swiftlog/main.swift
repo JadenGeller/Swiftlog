1e64363f8253caea36be8d02aecc1d3a171d5b9d
Jaden Geller <jaden.geller@gmail.com>
2016-01-19 02:27:26 -0800

    Builds, but with duplicate errors

---
+++ Swiftlog/Swiftlog/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Swiftlog
+//
+//  Created by Jaden Geller on 1/19/16.
+//  Copyright © 2016 Jaden Geller. All rights reserved.
+//
+
+print("HELLO")
+
+import Axiomatic
+
+let system = System(clauses: Array<Clause<Int>>())
